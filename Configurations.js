0830d01f882e6dde51567b42d48a55a0f0eaa27f
Krusherk <89709403+Krusherk@users.noreply.github.com>
2023-12-17 16:54:55 +0100

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can replace this number with yours //
+  gg = "2349169861242";   // You can replace this number with yours //
 }
 
 
